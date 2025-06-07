ad436c9e69275b1f21c32cd20443dbe53f40f679
Vilhelm <vilhelmroennenielsen@gmail.com>
2025-06-07 13:34:23 +0200

    Persons moved into a list

--- Actions.py
+++ Actions.py
@@ -9,19 +9,18 @@ world={
     "letter":{(-1,0):"u",(1,0):"d",(0,-1):"l",(0,1):"r"},
     "area":[[". ","P ",". ",". ",". ",". ",],
         [". ","O ",". ",". ",". ",". ",],
-        [". ","M ","O ",". ",". ",". ",],
+        [". ",". ","O ",". ",". ",". ",],
         [". ",". ",". ",". ",". ",". ",],
         [". ",". ",". ",". ",". ",". ",],
         [". ",". ",". ",". ",". ",". ",]],}
 
 class Person:
-    def __init__(self, coordinate:list, health:int, inventory:list, equipped:list, name:str,world:dict):
+    def __init__(self, coordinate:list, health:int, inventory:list, equipped:list, name:str):
         self.coordinate=coordinate
         self.health=health
         self.inventory=inventory
         self.equipped=equipped
         self.name=name
-        world["persons"][self.name]=self
     def move(self,area:dict,lenght):
         if not (0<=self.coordinate[0]+lenght[0]<=5 and 0<=self.coordinate[1]+lenght[1]<=5):
             return [self.coordinate[0],self.coordinate[1],"Out of bounds"]
@@ -72,14 +71,19 @@ class NPC(Person):
         if area[self.coordinate[0]][self.coordinate[1]-find_dir(diff[1])]==". ":
             return(letter[(0,-find_dir(diff[1]))])
         return(letter[(-find_dir(diff[0]),0)])
-player=Person([0,1],100,[],[1,6,2,1],"P ",world)
-monster=NPC([2,1],5,[],[1,4,1.5,1],"M ",world)
-
+world["persons"]["P "]=Person([0,1],100,[],[1,6,2,1],"P ")
+def spawn_npc(coordinate:list, health:int, inventory:list, equipped:list, name:str,world:dict):
+    world["persons"][name]=NPC(coordinate,health,inventory,equipped,name,)
+    world["area"][coordinate[0]][coordinate[1]]=name
+    return {"persons":world["persons"],"area":world["area"]}
+i=spawn_npc([2,1],5,[],[1,4,1.5,1],"M ",world)
+world["persons"]=i["persons"]
+world["area"]=i["area"]
 while True:
     print(generate_board(world["area"]))
     print("Make an action! Move Up, Down, Left, or Right, make an attack, pick up an item on the ground, or make an inventory interaction")
     s=input("u for up, d for down, l for left, r for right, a+the direction you're attacking in(u, d, l, or r) for attack, p for pick up, and i for inventory \n")
-    t=player.act(world,s,)
+    t=world["persons"]["P "].act(world,s,)
     print(t.pop("print"))
     if "del" in t:
         del world["persons"][t["del"]]
@@ -88,7 +92,7 @@ while True:
             world[i]=t[i]
     for i in list(world["persons"]):
         if i!="P ":
-            t=world["persons"][i].act(world,world["persons"][i].movement(world["letter"],world["area"],player,True))
+            t=world["persons"][i].act(world,world["persons"][i].movement(world["letter"],world["area"],world["persons"]["P "],True))
             for j in world:
                 if j in t:
                     world[j]=t[j]
